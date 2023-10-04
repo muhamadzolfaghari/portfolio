e72323903d5b83e4e47f006ce6361f4c18d299ef
muhamad.zolfaghari <you@example.com>
2023-10-04 19:31:02 +0330

    The App was changed and strings.ts was added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,11 @@
-import { Dialog, DialogContent } from "@mui/material";
-import Box from "@mui/material/Box";
-import useMediaQuery from "@mui/material/useMediaQuery";
 import React from "react";
-
-const App3 = () => (
-    <Box bgcolor={{ sm: "red", md: 'green' }}>Test</Box>
-)
-
-const App2 = () => {
-    const md = useMediaQuery((theme) => theme.breakpoints.down("sm"));
-    const sm = useMediaQuery((theme) => theme.breakpoints.down("sm"));
-
-    return <>
-        <Box sx={{ backgroundColor: sm ? "red" : "green" }}>
-            Hello
-        </Box>
-        <Dialog open fullScreen={sm}>
-            <DialogContent>
-            </DialogContent>
-        </Dialog>
-    </>
-}
+import { Grid } from "@mui/material";
 
 const App = () => (
-    <Box sx={(theme) => ({
-        [theme.breakpoints.down("md")]: {
-            backgroundColor: "green"
-        },
-        [theme.breakpoints.down("sm")]: {
-            backgroundColor: "red"
-        }
-    })}>Hello everyone!</Box>
-)
-
+  <Grid>
+    <Grid>test</Grid>
+    <Grid>test</Grid>
+  </Grid>
+);
 
 export default App;
---
+++ src/lib/strings.ts
@@ -0,0 +1,6 @@
+export const R = {
+    full_name: {
+        fa: 'محمد ذوالفقاری',
+        en: 'Mohammad Zolfaghari'
+    }
+}
