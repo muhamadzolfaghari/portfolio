c4db9af53b2430ed9ea806f33368b8a027a9c8d6
muhamad.zolfaghari <you@example.com>
2023-10-04 20:24:50 +0330

    public files were added.

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import theme from "./theme.const";
-import RTL from './Theme'
-import {CssBaseline, ThemeProvider} from "@mui/material";
+import RTL from "./Theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import App from "./App";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
-    document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 
 root.render(
-    <React.StrictMode>
-        <ThemeProvider theme={theme}>
-            <CssBaseline/>
-            <RTL>
-                <App/>
-            </RTL>
-        </ThemeProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RTL>
+        <App />
+      </RTL>
+    </ThemeProvider>
+  </React.StrictMode>,
 );
