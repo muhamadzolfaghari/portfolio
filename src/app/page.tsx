17fb54c2765027163ec5cec298320f839e8af327
muhamad <muhamad.zolfaghari@gmail.com>
2024-05-10 19:32:06 +0330

    remove some old code in hom epage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,7 @@
-"use client";
-import Image from "next/image";
-import { describe } from "node:test";
-import { useImperativeHandle, useRef } from "react";
-
-// network layer
-function post(
-  url: string,
-  data: File,
-  onprogress: (event: ProgressEvent) => void
-) {
-  return new Promise((resolve) => {
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", url, true);
-    xhr.upload.onprogress = onprogress;
-
-    xhr.onreadystatechange = (event) => {
-      if (xhr.readyState !== XMLHttpRequest.DONE) {
-        resolve(xhr);
-      }
-    };
-
-    const formData = new FormData();
-    formData.append("file", data);
-    xhr.send(formData);
-  });
-}
-
 export default function Home() {
-  const fileInput = useRef<HTMLInputElement>(null);
-  const percentageRef = useRef<HTMLDivElement>(null);
-
-  // functional kiss
-  // SOLID
-  // DRY
-  async function handleClick() {
-    // step
-    const res = await post(
-      "/upload",
-      fileInput.current!.files?.[0]!,
-      (event) => {
-        console.log(event);
-      }
-    );
-
-    console.log(res);
-
-    // OOP
-  }
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <label
-        className="border-2 border-dashed rounded-lg p-6 flex flex-col items-center cursor-pointer hover:bg-gray-100"
-        role="uploader"
-      >
-        <input
-          className="hidden"
-          id="file-input"
-          type="file"
-          ref={fileInput}
-          accept="*.jpg, *.png"
-        />
-      </label>
-      <div ref={percentageRef} className="color-green"></div>
-      <button onClick={handleClick}>upload</button>
+      <h1 className="text-4xl font-bold">Mohammad Zolfaghari</h1>
     </main>
   );
 }
