7bdcc010b56a66cd96a6144507f244998f9584fd
muhamad <muhamad.zolfaghari@gmail.com>
2024-05-14 00:17:05 +0330

    some tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 font-sans">
       <h1 className="text-4xl font-bold">Mohammad Zolfaghari</h1>
+      <div className="font-sans text-xl text-gray-700">
+        <p>
+          Passionate and results-driven, I am a Senior Front-End Developer with
+          over 7 years of experience in crafting cutting-edge B2B solutions
+          using the React ecosystem and a variety of programming languages. My
+          expertise extends to leading teams with strong leadership abilities,
+          mentoring effectively, and enhancing performance through rigorous
+          testing and efficient troubleshooting.
+        </p>
+        <p>
+          As a proactive problem solver, I am dedicated to constructing secure
+          and high-performing web applications. I thrive on continuous
+          improvement, committed to ongoing research for valuable insights that
+          inform our development processes. With a track record of delivering
+          impactful solutions, I am poised to contribute positively to dynamic
+          teams and projects.
+        </p>
+      </div>
     </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: [
@@ -7,11 +8,18 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      display: ['"Comfortaa"', ...defaultTheme.fontFamily.sans],
+      body: ['"Comfortaa"', ...defaultTheme.fontFamily.sans],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        sans: ["Comfortaa", ...defaultTheme.fontFamily.sans],
       },
     },
   },
